18209878567405018c693a3a1fdd94d62bc587a5
Leandro Reyes <38055233+leareyes22@users.noreply.github.com>
2024-10-07 22:26:56 -0300

    Sección 11 - Audio a texto (#8)

--- NestJS/nest-gpt/src/gpt/gpt.controller.ts
+++ NestJS/nest-gpt/src/gpt/gpt.controller.ts
@@ -1,19 +1,27 @@
 import {
   Body,
   Controller,
+  FileTypeValidator,
   Get,
   HttpStatus,
+  MaxFileSizeValidator,
   Param,
+  ParseFilePipe,
   Post,
   Res,
+  UploadedFile,
+  UseInterceptors,
 } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { Response } from 'express';
+import { diskStorage } from 'multer';
 import { GptService } from './gpt.service';
 import {
   OrthographyDto,
   ProConsDiscusserDto,
   TranslateDto,
   TextToAudioDto,
+  AudioToTextDto,
 } from './dtos';
 
 @Controller('gpt')
@@ -94,5 +102,36 @@ export class GptController {
     res.setHeader('Content-Type', 'audio/mp3');
     res.status(HttpStatus.OK);
     res.sendFile(filePath);
+  }
+
+  @Post('audio-to-text')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './generated/uploads',
+        filename: (req, file, callback) => {
+          const fileExtension = file.originalname.split('.').pop();
+          const fileName = `${new Date().getTime()}.${fileExtension}`;
+          return callback(null, fileName);
+        },
+      }),
+    }),
+  )
+  async audioToText(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({
+            maxSize: 1000 * 1024 * 5,
+            message: 'File is bigger than 5 MB.',
+          }),
+          new FileTypeValidator({ fileType: 'audio/*' }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+    @Body() audioToTextDto: AudioToTextDto,
+  ) {
+    return this.gptService.audioToText({ ...audioToTextDto, audioFile: file });
   }
 }
--- NestJS/nest-gpt/src/gpt/use-cases/index.ts
+++ NestJS/nest-gpt/src/gpt/use-cases/index.ts
@@ -4,3 +4,4 @@ export * from './prosConsDiscusserStream.use-case';
 export * from './translate.use-case';
 export * from './traslateStream.use-case';
 export * from './textToAudio.use-case';
+export * from './audioToText.use-case';
