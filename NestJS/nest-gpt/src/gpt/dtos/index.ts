94f91aac08c534a871beff6c06ecc96d68e03f03
Leandro Reyes <38055233+leareyes22@users.noreply.github.com>
2024-10-09 17:12:34 -0300

    Seccion 13: Backend - Generación de imágenes. (#10)

--- NestJS/nest-gpt/src/gpt/dtos/index.ts
+++ NestJS/nest-gpt/src/gpt/dtos/index.ts
@@ -3,3 +3,5 @@ export * from './proConsDiscusser.dto';
 export * from './translate.dto';
 export * from './textToAudio.dto';
 export * from './audioToText.dto';
+export * from './imageGeneration.dto';
+export * from './imageVariation.dto';
