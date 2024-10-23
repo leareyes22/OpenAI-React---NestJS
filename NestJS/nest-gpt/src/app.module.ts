05e79bd42f83e0dcb875740a15b6743bb49152ae
Leandro Reyes <38055233+leareyes22@users.noreply.github.com>
2024-10-22 21:49:42 -0300

    Sección 15: Backend - Asistentes de OpenAI (#12)

--- NestJS/nest-gpt/src/app.module.ts
+++ NestJS/nest-gpt/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { GptModule } from './gpt/gpt.module';
 import { ConfigModule } from '@nestjs/config';
+import { SamAssistantModule } from './sam-assistant/sam-assistant.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), GptModule],
+  imports: [ConfigModule.forRoot(), GptModule, SamAssistantModule],
 })
 export class AppModule {}
---
+++ NestJS/nest-gpt/src/sam-assistant/sam-assistant.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import OpenAI from 'openai';
+import {
+  checkCompleteStatusUseCase,
+  createMessageUseCase,
+  createRunUseCase,
+  createThreadUseCase,
+  getMessageListUseCase,
+} from './use-cases';
+import { QuestionDto } from './dtos/question.dto';
+
+@Injectable()
+export class SamAssistantService {
+  private openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+
+  async createThread() {
+    return await createThreadUseCase(this.openai);
+  }
+
+  async userQuestion(questionDto: QuestionDto) {
+    const { threadId, question } = questionDto;
+
+    await createMessageUseCase(this.openai, {
+      threadId,
+      question,
+    });
+
+    const run = await createRunUseCase(this.openai, { threadId });
+
+    await checkCompleteStatusUseCase(this.openai, { runId: run.id, threadId });
+
+    const messages = await getMessageListUseCase(this.openai, { threadId });
+
+    return messages.reverse();
+  }
+}
