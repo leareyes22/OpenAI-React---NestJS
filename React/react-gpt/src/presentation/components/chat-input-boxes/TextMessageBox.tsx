b7f335a3553dafec7f242661949ef4b0031ef87a
leareyes22 <leandroreyes_22@hotmail.com>
2024-11-02 12:04:47 -0300

    Restyling colors.

--- React/react-gpt/src/presentation/components/chat-input-boxes/TextMessageBox.tsx
+++ React/react-gpt/src/presentation/components/chat-input-boxes/TextMessageBox.tsx
@@ -32,7 +32,7 @@ export const TextMessageBox = ({
             type="text"
             autoFocus
             name="message"
-            className="flex w-full border rounded-xl text-gray-800 focus:outline-none focus:border-indigo-300 pl-4 h10"
+            className="flex w-full border rounded-xl text-gray-800 focus:outline-none focus:border-teal-400 pl-4 h10"
             placeholder={placeholder}
             autoComplete={disableCorrections ? "on" : "off"}
             autoCorrect={disableCorrections ? "on" : "off"}
--- React/react-gpt/src/presentation/layouts/DashboardLayout.tsx
+++ React/react-gpt/src/presentation/layouts/DashboardLayout.tsx
@@ -5,11 +5,11 @@ import { SidebarMenuItem } from "../components";
 export const DashboardLayout = () => {
   return (
     <main className="flex flex-row mt-7">
-      <nav className="hidden sm:flex flex-col ml-5 w-[370px] min-h-[calc(100vh-3.0rem)] bg-white bg-opacity-10 p-5 rounded-3xl">
+      <nav className="hidden sm:flex flex-col ml-5 w-[370px] min-h-[calc(100vh-3.0rem)] bg-teal-50 bg-opacity-10 p-5 rounded-3xl">
         <h1 className="font-bold text-lg lg:text-3xl bg-gradient-to-br from-white via-white/50 bg-clip-text text-transparent">
-          ReactGPT<span className="text-indigo-500">.</span>
+          ReactGPT<span className="text-teal-500">.</span>
         </h1>
-        <span className="text-xl">Bienvenido</span>
+        <span className="text-xl text-teal-50">Bienvenido</span>
 
         <div className="border-gray-700 border my-3" />
 
@@ -19,7 +19,7 @@ export const DashboardLayout = () => {
         ))}
       </nav>
 
-      <section className="mx-3 sm:mx-20 flex flex-col w-full h-[calc(100vh-50px)]  bg-white bg-opacity-10 p-5 rounded-3xl">
+      <section className="mx-3 sm:mx-20 flex flex-col w-full h-[calc(100vh-50px)]  bg-teal-100 bg-opacity-10 p-5 rounded-3xl">
         <div className="flex flex-row h-full">
           <div className="flex flex-col flex-auto h-full p-1">
             <Outlet />
